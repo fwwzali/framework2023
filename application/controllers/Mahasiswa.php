dbacf3f17ea709dc1d2f81f5abeda73807c1f65d
ewez <ewezali@gmail.com>
2023-09-19 14:14:07 +0700

    controller

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+
+class Mahasiswa extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('MahasiswaModel');
+    }
+
+    //menampilkan data mahasiswa
+    function index()
+    {
+        //ambil data dari model
+        $data['mhs'] = $this->MahasiswaModel->get_mahasiswa();
+        //ambil template tampilan dari view
+        $this->load->view('v_mahasiswa', $data);
+    }
+}
